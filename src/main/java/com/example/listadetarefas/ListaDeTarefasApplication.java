d5a9e83c3213a3b15ffe4548606e7c1fd4153513
matheusrezende995 <matheus995rezende@gmail.com>
2026-09-21 22:49:24 -0300

    Projeto base inicial da API de tarefas

---
+++ src/main/java/com/example/listadetarefas/ListaDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.example.listadetarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeTarefasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaDeTarefasApplication.class, args);
+    }
+
+}
